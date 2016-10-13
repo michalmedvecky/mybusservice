b2cd6ac362f25258e0a8d23a740d3e844c040802
Michal Medvecky <medvecky@pexe.so>
2016-10-13 12:04:01 +0200

    Add sentinel support, xml output for my endpoints

--- app.py
+++ app.py
@@ -5,73 +5,111 @@ import requests
 import pprint
 import redis
 import datetime
+from datetime import datetime
 import time
 from flask import Flask
 from flask import request,make_response
+from redis.sentinel import Sentinel
+import xml
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+import xml.dom.minidom
 
 nburl="http://webservices.nextbus.com/service/publicXMLFeed?command="
-app = Flask(__name__)
-red = redis.StrictRedis(host='localhost', port=6379, db=0)
 config_slow=-1 # Queries longer than this will be considered slow
+valid_endpoints=["/agencyList","/doesNotRunAtTime","/health-check","/slow-queries","/stats","/routeList","/routeConfig","/predictions","/predictionForMultiStops","/schedule","/messages","/vehicleLocations"]
+
+app = Flask(__name__)
+
+try:
+    # try connecting to local redis first
+    red = redis.StrictRedis(host='localhost', port=6379, db=0)
+    red.ping
+    rwdis=red
+    rodis=red
+
+except Exception as e:
+    try:
+        print("Trying to find Redis to connect to ...")
+        sentinel=Sentinel([("redis-sentinel", 26379)])
+        rwdis=sentinel.master_for("mymaster")
+        rodis=sentinel.slave_for("mymaster")
+        rwdis.get(None)
+        rodis.get(None)
+    except Exception as e:
+        print("Can't connect to any Redis, dying")
+        exit(1)
 
 def log_slow_request(url,t):
     if t>config_slow:
         timeofevent=time.strftime("%a, %d %b %Y %H:%M:%S GMT")
-        red.sadd("slowrequests",timeofevent+" Query "+url+" took "+str(t)+" milliseconds.")
+        rwdis.sadd("slowrequests",timeofevent+" Query "+url+" took "+str(t)+" milliseconds.")
 
 def cachepage(url):
     print("Requesting "+url)
-    red.incr("stats:validrequests", amount=1)
+    rwdis.incr("stats:validrequests", amount=1)
 
-    if red.exists(url):
-        return red.get(url)
+    if rodis.exists(url):
+        return rodis.get(url)
     else:
         print("Getting "+url)
         r=requests.get(url)
         print("Status code: "+str(r.status_code))
-        red.set(url,r.content, ex=180000)
+        rwdis.set(url,r.content, ex=180000)
         return r.content
 
 def myresponse(url):
-    expiry_time = datetime.timedelta(0,red.ttl(url)) + datetime.datetime.utcnow()
+    expiry_time = datetime.timedelta(0,rodis.ttl(url)) + datetime.datetime.utcnow()
     response = make_response(cachepage(url))
     response.mimetype = "text/plain"
     response.headers["Expires"] = expiry_time.strftime("%a, %d %b %Y %H:%M:%S GMT")
     response.code=200
     return response
 
+@app.route("/stats")
+def stats():
+    rwdis.incr("requests:/stats", amount=1)
+    out = Element("body", attrib={})
+    for e in valid_endpoints:
+        ep = SubElement(out, 'endpoint', attrib={'url':e,'accesscount':str(rodis.get("requests:"+e))})
+    response=xml.dom.minidom.parseString(tostring(out))
+    return response.toprettyxml()
+
 @app.route("/doesNotRunAtTime/<int:hour>")
 def doesnotrunattime(hour):
     t_start=time.time()
-    if red.exists("nonruntimes:"+str(hour)):
-        response=make_response(str(red.smembers("nonruntimes:"+str(hour))))
+    if rodis.exists("nonruntimes:"+str(hour)):
+        body = Element("body")
+        lnorun = SubElement(body, "doesnotrunat", attrib={"oclock":str(hour)})
+        for member in rodis.smembers("nonruntimes:"+str(hour)):
+            agency,route=str.split(member,":")
+            xmember = SubElement(lnorun, "route", attrib={"agency":agency,"tag":route})
+        response=make_response(xml.dom.minidom.parseString(tostring(body)).toprettyxml())
         log_slow_request(request.url,time.time()-t_start)
         return response
 
-    if not red.exists("agencies"):
+    if not rodis.exists("agencies"):
         r = cachepage("http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList")
         e = xml.etree.ElementTree.fromstring(r)
 
         for agency in e.findall('agency'):
             a=agency.get('tag')
             print("Adding "+a)
-            red.sadd("agencies",a)
-        red.expire("agencies", 86400) # I guess agencies dont' update more frequently than daily
+            rwdis.sadd("agencies",a)
+        rwdis.expire("agencies", 86400) # I guess agencies dont' update more frequently than daily
 
-    #    pprint.pprint(red.smembers("agencies"))
-    for a in red.smembers("agencies"):
-        if not red.exists(a+":routes"):
+    for a in rodis.smembers("agencies"):
+        if not rodis.exists(a+":routes"):
             print("getting route list for "+a)
             r = cachepage("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a="+a)
             routes=xml.etree.ElementTree.fromstring(r)
             print("parsing route list...")
             for route in routes.findall('route'):
                 print("Adding route "+route.get('tag')+" for agency "+a)
-                red.sadd(a+":routes",route.get('tag'))
-            red.expire(a+":routes",86400)
+                rwdis.sadd(a+":routes",route.get('tag'))
+            rwdis.expire(a+":routes",86400)
 
-    for a in red.smembers("agencies"):
-        for r in red.smembers(a+":routes"):
+    for a in rodis.smembers("agencies"):
+        for r in rodis.smembers(a+":routes"):
             print("Processing "+r)
     #            print("Getting schedule of route "+r+" of agency "+a)
             out = cachepage("http://webservices.nextbus.com/service/publicXMLFeed?command=schedule&a="+a+"&r="+r)
@@ -86,18 +124,19 @@ def doesnotrunattime(hour):
 
             for h in range(0, 24):
                 if h not in runtimes:
-                    red.sadd("nonruntimes:"+str(h),a+":"+r)
-                    red.expire("nonruntimes:"+str(h), 86400)
-    response=make_response(str(red.smembers("nonruntimes:"+str(hour))))
+                    rwdis.sadd("nonruntimes:"+str(h),a+":"+r)
+                    rwdis.expire("nonruntimes:"+str(h), 86400)
+    response=make_response(str(rodis.smembers("nonruntimes:"+str(hour))))
     log_slow_request(request.url,time.time()-t_start)
     return response
 
 
 @app.route('/health-check')
 def health_check():
-    red.incr("requests:/health-check", amount=1)
+    rwdis.incr("requests:/health-check", amount=1)
     try:
-        response = red.client_list()
+        response = rodis.client_list()
+        response = rwdis.client_list()
         response = make_response("OK\n")
         response.code=200
 
@@ -109,14 +148,14 @@ def health_check():
 
 @app.route('/routeList/<string:agency>')
 def myrouteList(agency):
-    red.incr("requests:/routeList")
+    rwdis.incr("requests:/routeList")
     url=nburl+"routeList&a="+agency
     return myresponse(url)
 
 @app.route('/routeConfig/<string:agency>/<string:route>')
 def routeConfig(agency,route):
-    red.incr("requests:/routeConfig", amount=1)
-    red.incr("stats:validrequests", amount=1)
+    rwdis.incr("requests:/routeConfig", amount=1)
+    rwdis.incr("stats:validrequests", amount=1)
     url=nburl+"routeConfig&a="+agency+"&r="+route
     return myresponse(url)
 
@@ -130,7 +169,7 @@ def routeConfig(agency,route):
 @app.route('/routeConfig')
 def proxyHandler():
     t_start=time.time()
-    red.incr("requests:"+request.path, amount=1)
+    rwdis.incr("requests:"+request.path, amount=1)
     url=nburl+request.path[1:]+"{q}".format(q="&" if len(request.query_string)>0 else "")+request.query_string
     contents=myresponse(url)
     log_slow_request(request.url,time.time()-t_start)
@@ -139,7 +178,7 @@ def proxyHandler():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def notfound(path):
-    red.incr("stats:invalidrequests", amount=1)
+    rwdis.incr("stats:invalidrequests", amount=1)
     response=make_response("Not found\n")
     response.code=404
 #    return 'You want path: %s \n' % path+" "+request.method
